3bcc90bd295e16adb57a931b7cdbec3ab1197f79
Peter Hurd <peter.hurd@gmail.com>
2014-02-23 16:37:02 -0500

    Fixed JSON structure for sample
    Corrected JSON format for Childcare sample

--- ChildcareCenters.js
+++ ChildcareCenters.js
@@ -1,41 +1,54 @@
-var ChildCareCenters =  
-	{"centers": 
-	[
-		{"type": "Feature",
-			"properties": {
-				"name": "VNA Learning Together Child Care",
-				"quality": "5",
-				"Capacity05": "17",
-				"CenterLocation": "center"
-				"popupContent": "VNA Learning Together Child Care"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-73.214215, 44.483694]
-			}
-		},
-		{"type": "Feature",
-			"properties": {
-				"name": "Burlington Children's Space, Inc.",
-				"quality": "5",
-				"Capacity05": "46",
-				"CenterLocation": "center"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-73.20922, 44.4868]
-			}
-		}
-		{"type": "Feature",
-			"properties": {
-				"name": "Christ the King Pre-School and Childcare Center",
-				"quality": "2",
-				"Capacity05": "20",
-				"CenterLocation": "center"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-73.211147, 44.463066]
-			}			
-		}
-	]
+var childcareCenters = 
+{
+    "centers": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "VNA Learning Together Child Care",
+                "quality": "5",
+                "Capacity05": "17",
+                "CenterLocation": "center",
+                "popupContent": "VNA Learning Together Child Care"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -73.214215,
+                    44.483694
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Burlington Children's Space, Inc.",
+                "quality": "5",
+                "Capacity05": "46",
+                "CenterLocation": "center"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -73.20922,
+                    44.4868
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Christ the King Pre-School and Childcare Center",
+                "quality": "2",
+                "Capacity05": "20",
+                "CenterLocation": "center"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -73.211147,
+                    44.463066
+                ]
+            }
+        }
+    ]
+}
