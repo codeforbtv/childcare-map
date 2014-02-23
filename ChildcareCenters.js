ad6b4d4d9c4d9b582c74a1f7c0db2de8d1e87205
Peter Hurd <peter.hurd@gmail.com>
2014-02-23 16:15:33 -0500

    New childcare center data file
    3 centers with some attributes

---
+++ ChildcareCenters.js
@@ -0,0 +1,41 @@
+var ChildCareCenters =  
+	{"centers": 
+	[
+		{"type": "Feature",
+			"properties": {
+				"name": "VNA Learning Together Child Care",
+				"quality": "5",
+				"Capacity05": "17",
+				"CenterLocation": "center"
+				"popupContent": "VNA Learning Together Child Care"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-73.214215, 44.483694]
+			}
+		},
+		{"type": "Feature",
+			"properties": {
+				"name": "Burlington Children's Space, Inc.",
+				"quality": "5",
+				"Capacity05": "46",
+				"CenterLocation": "center"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-73.20922, 44.4868]
+			}
+		}
+		{"type": "Feature",
+			"properties": {
+				"name": "Christ the King Pre-School and Childcare Center",
+				"quality": "2",
+				"Capacity05": "20",
+				"CenterLocation": "center"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-73.211147, 44.463066]
+			}			
+		}
+	]
